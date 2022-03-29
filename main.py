e1cfc2dcb9cb41886e372ee7c9bda6e724a2c116
ufuk devletli <devletli@gmail.com>
2022-03-29 14:12:08 +0300

    rect

---
+++ main.py
@@ -0,0 +1,13 @@
+import turtle
+myturtle = turtle.Turtle()
+myturtle.penup()
+myturtle.goto(50,75)
+myturtle.pendown()
+myturtle.forward(200)
+myturtle.left(90)
+myturtle.forward(200)
+myturtle.left(90)
+myturtle.forward(200)
+myturtle.left(90)
+myturtle.forward(200)
+turtle.done()
